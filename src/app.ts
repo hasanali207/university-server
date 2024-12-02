7b2a801f83a84e3b1e953151dbc2ef18ad12aae3
hasanali207 <hasanali142228@gmail.com>
2024-12-02 22:02:41 +0600

    added data with changed

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import { StudentRoutes } from './app/modules/student/student.route';
 
-const app: Application = express();
+// const app: Application = express();
 
 //parsers
 app.use(express.json());
